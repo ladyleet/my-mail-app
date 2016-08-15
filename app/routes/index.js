5d3266ffafd285b653a442ab1a1a07a16428397a
Tracy Lee <tracy@wingman.events>
2016-08-15 14:57:22 -0700

    brain melt

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.modelFor('folder');
+  beforeModel() {
+    this.replaceWith('folder', 'inbox');
   }
 });
